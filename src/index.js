e7466e1d226db79dbc63a7b13ba55f07e963d262
Marcelo Abrão <masilvasql@gmail.com>
2022-05-28 19:53:09 -0300

    criação de middlewares

--- src/index.js
+++ src/index.js
@@ -10,21 +10,87 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  
+  const user = users.find(user=> user.username === username);
+  
+  if(!user){
+     return response.status(404).json({error:'User not found'})
+  }
+
+  request.user = user;
+
+  next();
+  
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+ 
+   const {username} = request.headers;
+  
+
+
+  const user = users.find(user=> user.username === username) || request.user;
+  
+  if(!user){
+     return response.status(404).json({error:'User not found'})
+  }
+
+  if(user.todos.length > 0 ){
+    if((user.todos.length <= 10 && !user.pro) || user.pro){
+      next();
+    }
+  }
+
+  next()
+  
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const{username} = request.headers;
+  const {id} = request.params;
+
+  const user = users.find(user=> user.username === username);
+ 
+  if(!user){
+    return response.status(404).json({error:'User Not Found!'})
+  }
+  let todo = null;
+  if(user.todos){
+    if(!validate(id)){
+      return response.status(400);
+    }
+
+    todo = user.todos.find(todo=> todo.id === id);
+    if(!todo){
+      return response.status(404).json({error:"Todo not found or not belong to this user"})
+    }
+    
+    
+  }
+  
+  request.todo = todo;
+  request.user = user;
+
+  next();
+
+  
 }
+
 
 function findUserById(request, response, next) {
-  // Complete aqui
-}
+  const {id} = request.params;
 
+  const user = users.find(user=> user.id === id)
+
+  if(!user){
+    return response.status(404).json({error:'User not found!'})
+  }
+
+  request.user = user;
+
+  next();
+}
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
@@ -112,7 +178,7 @@ app.delete('/todos/:id', checksExistsUserAccount, checksTodoExists, (request, re
   const todoIndex = user.todos.indexOf(todo);
 
   if (todoIndex === -1) {
-    return response.status(404).json({ error: 'Todo not found' });
+    return response.status(204).json({ error: 'Todo not found' });
   }
 
   user.todos.splice(todoIndex, 1);
